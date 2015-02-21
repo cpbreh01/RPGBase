db9b493fac572b09b0c053c708c6eef5b04cb8e3
chris <cpbreh01@gmail.com>
2015-02-20 20:29:16 -0500

    Initiall Commit

---
+++ RPGBase/Classes/Character.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RPGBase.Classes
+{
+    class Character
+    {
+        string name;
+        string race;
+        int hitPoints;
+        int initative;
+        int speed;
+        ArmorClass CharacterArmorClass;
+        
+        //Base Attributes
+        int constitution;
+        int dexterity;
+        int charisma;
+        int strength;
+        int wisdom;
+        int intellegence;
+
+        Alignment CharacterAlignment;
+
+
+    }
+
+    enum Alignment {LawfulGood,NeutralGood,ChaoticGood,LawfulNeutral,TrueNeutral,ChaoticNeutral,LawfulEvil,NeutralEvil,ChaoticEvil}
+
+    class Attack
+    {
+        int AttackModifier;
+        int Damage;
+        //To Do add effects
+    }
+    
+
+    class Skills
+    {
+        string SkillName;
+    }
+
+    class ArmorClass
+    {
+
+    }
+}
