ef12019648f6bfc7abe856d836c1e352219473fa
dsschuene <dsschuene@gmail.com>
2015-02-21 13:32:47 -0500

    Implemented Armor Class
    Made different ACs

--- RPGBase/Classes/ArmorClass.cs
+++ RPGBase/Classes/ArmorClass.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace RPGBase.Classes
 {
+    enum ArmorModTypes { ArmorBonus, ShieldBonus, DexModifier, SizeModifier, NaturalArmor, DeflectionModifier, Micellaneous }
+
+
     class ArmorClass
     {
+        const int baseArmor = 10;
+        List<ArmorModifiers> ACMods = new List<ArmorModifiers>();
+
+        public int generateAC()
+        {
+            return baseArmor + ACMods.Sum(r => r.value);
+        }
+
+        public int generateFlatFootedAC()
+        {
+            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.DexModifier).Sum(r => r.value);
+        }
+
+        public int generateTouchAC()
+        {
+            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.ArmorBonus).Sum(r => r.value);
+        }
     }
+
+    class ArmorModifiers{
+        public ArmorModTypes type; //include Dex, Armor,
+        public string typeName;
+        public int    value;
+    }
+
 }
