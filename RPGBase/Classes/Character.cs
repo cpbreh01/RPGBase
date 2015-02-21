5fe7619d680ee4572338356506294c2eee239529
chris <cpbreh01@gmail.com>
2015-02-21 13:02:35 -0500

    Made character class more well formed'

--- RPGBase/Classes/Character.cs
+++ RPGBase/Classes/Character.cs
@@ -5,27 +5,115 @@ using System.Text;
 
 namespace RPGBase.Classes
 {
-    class Character
+    public class Character
     {
-        string name;
-        string race;
-        int hitPoints;
-        int initative;
-        int speed;
-        ArmorClass CharacterArmorClass;
-        
-        //Base Attributes
-        int constitution;
-        int dexterity;
-        int charisma;
-        int strength;
-        int wisdom;
-        int intellegence;
+        //Note int is an unsigned
 
-        Alignment CharacterAlignment;
+        private string name;
+        private string race;
+        private int hitPoints;
+        private int initative;
+        private int speed;
+        private ArmorClass characterArmorClass;
+        public Attribute characterAttributes;
+        private Alignment characterAlignment;
 
+        //public Character(string _Name, string _Race, string _BaseHp, int _Initative, int  )
+        //{
+
+        //}
+
+        //To do: Determine which properties in this list need to be readonly. (i.e. only set in object creation or through methods.)
+        #region Properties
+
+            public string Name
+            {
+                get { return name; }
+                set { name = value; }
+            }
+            public string Race
+            {
+                get { return race; }
+            }
+            public Alignment CharacterAlignment
+            {
+                get { return characterAlignment; }
+                set { characterAlignment = value; }
+            }
+            public int Speed
+            {
+                get { return speed; }
+                set { speed = value; }
+            }
+            public int HitPoints
+            {
+                get { return hitPoints; }
+                set { hitPoints = value; }
+            }
+            public int Initative
+            {
+                get { return initative; }
+                set { initative = value; }
+            }
+
+            internal ArmorClass CharacterArmorClass
+            {
+                get { return characterArmorClass; }
+                set { characterArmorClass = value; }
+            }
+
+    #endregion
 
     }
+
+    public class Attribute
+    {
+        //Base Attributes
+        private int constitution;
+        private int dexterity;
+        private int charisma;
+        private int strength;
+        private int wisdom;
+        private int intellegence;
+
+        //Other Properties
+        public int Constitution
+        {
+            get { return constitution; }
+            set { constitution = value; }
+        }
+        public int Dexterity
+        {
+            get { return dexterity; }
+            set { dexterity = value; }
+        }
+        public int Charisma
+        {
+            get { return charisma; }
+            set { charisma = value; }
+        }
+        public int Strength
+        {
+            get { return strength; }
+            set { strength = value; }
+        }
+        public int Wisdom
+        {
+            get { return wisdom; }
+            set { wisdom = value; }
+        }
+        public int Intellegence
+        {
+            get { return intellegence; }
+            set { intellegence = value; }
+        }
+
+        public void GenerateModifiers()
+        {
+
+        }
+    }
+
 
     enum Alignment {LawfulGood,NeutralGood,ChaoticGood,LawfulNeutral,TrueNeutral,ChaoticNeutral,LawfulEvil,NeutralEvil,ChaoticEvil}
 
@@ -40,10 +128,5 @@ namespace RPGBase.Classes
     class Skills
     {
         string SkillName;
-    }
-
-    class ArmorClass
-    {
-
     }
 }
