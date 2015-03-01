2296ec729b56c21ddd71743136c3138bf29bfdbc
chris <cpbreh01@gmail.com>
2015-03-01 10:49:36 -0500

    initall commit for Attribute consolidation branch. Added more comments and started combining attrbutes to make modifer management easier.

--- RPGBase/Classes/ArmorClass.cs
+++ RPGBase/Classes/ArmorClass.cs
@@ -7,7 +7,8 @@ namespace RPGBase.Classes
 {
     //You armor class is how hard your character is to hit.
 
-    public enum ArmorModTypes { ArmorBonus, ShieldBonus, DexModifier, SizeModifier, NaturalArmor, DeflectionModifier, Micellaneous }
+    //CB - 3-1-15 Moved this enum to character page where I am trying to Define all modifiers. Added ArmorModType NotArmor so that we that I could combine all mods.
+    //public enum ArmorModTypes { ArmorBonus, ShieldBonus, SizeModifier, NaturalArmor, DeflectionModifier, Micellaneous }
 
 
     public class ArmorClass
@@ -15,19 +16,25 @@ namespace RPGBase.Classes
         const int baseArmor = 10;
         List<ArmorModifiers> ACMods = new List<ArmorModifiers>();
 
-        public int generateAC()
+        public int generateAC(int DexModifier)
         {
-            return baseArmor + ACMods.Sum(r => r.value);
+            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.NotArmor).Sum(r => r.value) + DexModifier;
         }
 
         public int generateFlatFootedAC()
         {
-            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.DexModifier).Sum(r => r.value);
+            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.NotArmor).Sum(r => r.value);
         }
 
-        public int generateTouchAC()
+        public int generateTouchAC(int DexModifier)
         {
-            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.ArmorBonus).Sum(r => r.value);
+            return baseArmor + ACMods.Where(r => r.type != ArmorModTypes.ArmorBonus && r.type != ArmorModTypes.NotArmor).Sum(r => r.value) + DexModifier;
+        }
+
+        //CB - 3-1-15 - TODO: Return value for speed bonus or penalty. Heavy armor will make a character move more slowly.
+        public int generateSpeedPenaltyOrBonus()
+        {
+            return 0;
         }
     }
 
--- RPGBase/Classes/Character.cs
+++ RPGBase/Classes/Character.cs
@@ -6,86 +6,247 @@ using System.Text;
 namespace RPGBase.Classes
 {
 
-    //TODO: We may want to make RACE and CLASS into custom class objects. That or because CLASS and RACE are determined after 
-    //doing the ability rolls.
-    //Another option may be to create a custom method to implement a character class and race. (AddCLASS add AddRACE) This would of course add Attribute modifiers
-    //- CB2_25_15
-
-    //RACES: Human, Dwarf, Elf, Gnome, Halfling, Half-Elf, Half-Orc, Monster(Maybe)
-    //Class: Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rouge, Sorcerer, Wizard. (We should add Tourist and Samurai as an homage to Nethack)
+    //RACES: Human, Dwarf, Elf, Gnome, Halfling, Half-Elf, Half-Orc
+    //Class: Barbarian, Bard, Cleric, Druid, Fighter, Monk, Paladin, Ranger, Rouge, Sorcerer, Wizard
 
 
     public class Character
     {
         private string name;
-        private int age;                            //Added 2-25-15 - Age depending on Race effects certain Attributes.
-        private string gender;                      //Added 2-25-15
-        private string race;
-        private int hitPoints;                      //Class Base + Constitution modifier.
-        private int initative;                      //TDexterity Modifier (+ any feat bonus)
-        private int speed;
+        private int age;                            //CB - 2-25-15 - Age depending on Race affects certain Attributes. (Ex a human at the age of 30 will get -1 strength, con, and +1 Wisdom etc.)
+        private string gender;                      //CB - 2-25-15 - This is the Gender of the Character
+        private string race;                        //Race determines modifiers on rolls for attributes.
         private Alignment characterAlignment;
+        public List<Languages> characterLanguagesSpoken;    //CB - 3/1/15 - These are the languages the character speaks this is determined initially by race and intelligence attribute..
 
-        //TODO: Create Level Class
-            //Apparently every 4th level allows you to add 1 point to any attribute.
+        public Attributes CharacterAttributes; //CB - 3-1-15 - Consolidated Attributes and armor class into one class so that it would be easier to manage modifiers.
+        public List<Skill> Skills;
+
+        //CB - 2-25-15 - TODO: Figure out how to manager level ups/downs
+            //Every 4th level allows you to add 1 point to any attribute.
             //Every level results in your rolling a "Hit die" to determine how many hit points you gain.
-        //TODO: Figure out how to track ATTACK BONUS - this is determined by Class Base + Strength modifier
+            //Level up xp requirements are defined by class.
+        //CB - 2-25-15 - TODO: Create FEAT class
 
-        //Instead of having Attributes listed separately, it may be better to have them contained within an object. That way modifiers can all be in the same place without us having to assign them.
+        private int baseHitPoints;                          //Hit Points = Class Base + Constitution modifier.
+        private int currentHitPoints;
+        private int baseAttack;                             //Base Attack = Class Base + Strength modifier
+        private int baseFortitudeSave;                      //Fortitude = Class base + Constitution Modifier
+        private int baseReflexSave;                         //Reflex = Class base + Dexterity Modifier
+        private int baseWillSave;                           //Will = Class base + Wisdom Modifier
+        private int movementSpeed;                          //Speed = Racial Speed + ArmorPenalty/Bonus
+
+        /// <summary>
+        /// Hit Points = Class Base + Constitution modifer
+        /// </summary>
+        public int MaxHitPoints
+        {
+            get
+            {
+                return baseHitPoints + CharacterAttributes.CalcConstitutionModifer;
+            }
+        }
+        /// <summary>
+        /// Initiative = DexterityModifer + Any Feat Bonus
+        /// </summary>
+        public int Initative
+        {
+            get
+            {
+                return CharacterAttributes.CalcDexterityModifer;    //ToDo Find a way to add in Feat Bonuses here?
+            }
+        }
+        /// <summary>
+        /// Base Attack = Class Base + Strength modifier
+        /// </summary>
+        public int BaseAttack
+        {
+            get
+            {
+                return baseAttack + CharacterAttributes.CalcStrengthModifer;  
+            }
+        }
+        /// <summary>
+        /// Fortitude = Class base + Constitution Modifier
+        /// </summary>
+        public int Fortitude
+        {
+            get
+            {
+                return baseFortitudeSave + CharacterAttributes.CalcConstitutionModifer; 
+            }
+        }
+        /// <summary>
+        /// Reflex = Class base + Dexterity Modifier
+        /// </summary>
+        public int Reflex
+        {
+            get
+            {
+                return baseReflexSave + CharacterAttributes.CalcDexterityModifer; 
+            }
+        }
+        /// <summary>
+        /// Will = Class base + Wisdom Modifier
+        /// </summary>
+        public int Will
+        {
+            get
+            {
+                return baseWillSave + CharacterAttributes.CalcWisdomModifer; 
+            }
+        }
+        /// <summary>
+        /// Speed = Racial Speed + ArmorPenalty/Bonus
+        /// </summary>
+        public int Speed
+        {
+            get
+            {
+                return movementSpeed + CharacterAttributes.CalcSpeedModifer; 
+            }
+        }
+
+        /// <summary>
+        /// This is the Characters Name
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        /// <summary>
+        /// This is the Characters Race. A race confers certain bonuses which are used to initially set a characters Attributes and skills
+        /// </summary>
+        public string Race
+        {
+            get { return race; }
+        }
+        /// <summary>
+        /// This is the Characters Alignment. I.e. how they act.
+        /// </summary>
+        public Alignment CharacterAlignment
+        {
+            get { return characterAlignment; }
+            set { characterAlignment = value; }
+        }
+        /// <summary>
+        /// This is the characters current number of hit points.
+        /// CB - 3-1-15
+        /// </summary>
+        public int CurrentHitPoints
+        {
+            get
+            {
+                return currentHitPoints;
+            }
+            set
+            {
+                if (value > MaxHitPoints)
+                {
+                    currentHitPoints = MaxHitPoints;
+                }
+                else
+                {
+                    currentHitPoints = value;
+                }
+
+            }
+        }
+
+    }
+
+
+    //TODO: by combining Attributes into one class we are trying to consolidate Modifiers so we don't have to manage them across multiple objects.
+    public class Attributes
+    {
         public Attribute Strength;
         public Attribute Dexterity;
         public Attribute Constitution;
         public Attribute Intelligence;
         public Attribute Charisma;
         public Attribute Wisdom;
-
         public ArmorClass CharacterArmorClass;
-        public Save Fortitude;                      //Class base + Constitution Modifier
-        public Save Reflex;                         //Class base + Dexterity Modifier
-        public Save Will;                           //Class base + Wisdom Modifier
-
-        List<Skill> Skills = new List<Skill>();
-
-        //TODO: Create FEAT class - CB2_25_15
 
 
-        #region Properties
-
-            public string Name
+        public int CalArmorClass
+        {
+            get
             {
-                get { return name; }
-                set { name = value; }
+                return CharacterArmorClass.generateAC(CalcDexterityModifer);
             }
-            public string Race
+        }
+        public int CalFlatFootedArmorClass
+        {
+            get
             {
-                get { return race; }
+                return CharacterArmorClass.generateFlatFootedAC();
             }
-            public int HitPoints
+        }
+        public int CalTouchArmorClass
+        {
+            get
             {
-                get { return hitPoints; }
-                set { hitPoints = value; }
+                return CharacterArmorClass.generateTouchAC(CalcDexterityModifer);
             }
-            public int Initative
+        }
+        public int CalcStrengthModifer
+        {
+            get
             {
-                get { return initative; }
-                set { initative = value; }
+                return Strength.getAttributeModifier();
             }
-            public int Speed
+        }
+        public int CalcDexterityModifer
+        {
+            get
             {
-                get { return speed; }
-                set { speed = value; }
+                return Dexterity.getAttributeModifier();
             }
-            public Alignment CharacterAlignment
+        }
+        public int CalcConstitutionModifer
+        {
+            get
             {
-                get { return characterAlignment; }
-                set { characterAlignment = value; }
+                return Constitution.getAttributeModifier();
             }
-
-        #endregion
+        }
+        public int CalcIntelligenceModifer
+        {
+            get
+            {
+                return Intelligence.getAttributeModifier();
+            }
+        }
+        public int CalcCharismaModifer
+        {
+            get
+            {
+                return Charisma.getAttributeModifier();
+            }
+        }
+        public int CalcWisdomModifer
+        {
+            get
+            {
+                return Wisdom.getAttributeModifier();
+            }
+        }
+        public int CalcSpeedModifer
+        {
+            get
+            {
+                return CharacterArmorClass.generateSpeedPenaltyOrBonus(); //CB - 3-1-15 - Do we need to add in whether character is overburdened
+            }
+        }
     }
 
-    public enum AttributeType { Int = "Intelligence", Dex = "Dexterity", Con = "Constitution", Cha = "Charisma", Wis = "Wisdom", Str = "Strength" };
 
+    //TODO: try to combine modifiers into one class so that they are easier to manage.
+
+    public enum AttributeType { Int = "Intelligence", Dex = "Dexterity", Con = "Constitution", Cha = "Charisma", Wis = "Wisdom", Str = "Strength" };
+   
+    public enum ArmorModTypes { NotArmor, ArmorBonus, ShieldBonus, SizeModifier, NaturalArmor, DeflectionModifier, Micellaneous }
     public class MiscModifer
     {
         string ModifierName;
@@ -100,5 +261,12 @@ namespace RPGBase.Classes
 
     }
 
+    /// <summary>
+    /// Object that holds details of a language.
+    /// </summary>
+    public class Languages
+    {
+        string LanguageName;
+    }
     public enum Alignment {LawfulGood,NeutralGood,ChaoticGood,LawfulNeutral,TrueNeutral,ChaoticNeutral,LawfulEvil,NeutralEvil,ChaoticEvil}
 }
